836836ce37682da318f70c91dcbf33be16eb3639
Michael <michael@mchang.name>
2021-08-17 20:56:23 -0400

    finished decision tree

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ var PAGES = [{
     'text': 'Is the video better off without skipping the segment?',
     'type': 'yesno',
     'targets': [
-        1, 2 // dns, root 2
+        1, 2
     ]
 }, {
     'text': "Do not submit anything"
@@ -46,12 +46,196 @@ var PAGES = [{
     'text': 'Is the segment shorter than one second? Is the transition really bad?',
     'type': 'yesno',
     'targets': [
-        1, 3 // dns, root 3
+        1, 3
     ]
 }, {
     'text': 'Is there any content in this segment?',
     'type': 'yesno',
     'targets': [
-        // ahhh
+        12, 4
     ]
+}, {
+    'text': 'Is there a sponsor in the segment?',
+    'type': 'yesno',
+    'targets': [
+        16, 5
+    ]
+}, {
+    'text': 'Are they asking you to live, subscribe, turn on notifications, comment and share?',
+    'type': 'yesno',
+    'targets': [
+        23, 6
+    ]
+}, {
+    'text': 'Are they promoting something',
+    'type': 'yesno',
+    'targets': [
+        26, 7
+    ]
+}, { 
+    'text': 'Does this segment contain an animation or still frame? (May contain audio)',
+    'type': 'yesno',
+    'targets': [
+        29, 8
+    ]
+}, {
+    'text': 'Is this a unique spoken introduction? Or a unique skit?',
+    'type': 'yesno',
+    'targets': [
+        1, 9
+    ]
+}, {
+    'text': 'Is this a time-lapse or slowmo clip? Is it showing preperation/ cleanup of some sort?',
+    'type': 'yesno',
+    'targets': [
+        1, 10
+    ]
+}, {
+    'text': 'Is music the primary aspect of the video',
+    'type': 'yesno',
+    'targets': [
+        36, 1
+    ]
+}, {
+    'text': 'Is this video part of a series',
+    'type': 'yesno',
+    'targets': [
+        12, 1
+    ]
+}, {
+    'text': 'Does this segment appear in the next or previous videos?',
+    'type': 'yesno',
+    'targets': [
+        14, 1
+    ]
+}, {
+    'text': 'Does content from this segment appear later or earlier in this video?',
+    'type': 'yesno',
+    'targets': [
+        14, 11
+    ]
+}, {
+    'text': 'Is there a spoken summary that is not present in the other appearance?',
+    'type': 'yesno',
+    'targets': [
+        1, 15
+    ]
+}, {
+    'text': 'Preview/ Recap'
+}, {
+    'text': 'Is the video primarily about the sponsor or could this be cause of bias?',
+    'type': 'yesno',
+    'targets': [
+        1, 17
+    ]
+}, {
+    'text': 'Is it obvious the sponsor is owned by the same company/person',
+    'type': 'yesno',
+    'targets': [
+        25, 18
+    ]
+}, {
+    'text': 'Is it obvious the creator is being paid to promote this? (or received free stuff)',
+    'type': 'yesno',
+    'targets': [
+        19, 25
+    ]
+}, {
+    'text': 'Is the sponsor Nebula or Curiosity Stream',
+    'type': 'yesno',
+    'targets': [
+        20, 39
+    ]
+}, {
+    'text': 'Does the creator have exclusive videos on the platform',
+    'type': 'yesno',
+    'targets': [
+        21, 39
+    ]
+}, {
+    'text': 'Do they switch between promoting the platform and their exclusives rapidly',
+    'type': 'yesno',
+    'targets': [
+        39, 22
+    ]
+}, {
+    'text': 'When promoting their exclusive: self promo, While promoting the platform: sponsor',
+}, {
+    'text': 'Do they rapidly switch between reminding you to interact and self promotion?',
+    'type': 'yesno',
+    'targets': [
+        25, 24
+    ]
+}, {
+    'text': 'Interaction Reminder'
+}, {
+    'text': 'Are endcards present or would outro make more sense if it was included',
+    'type': 'yesno',
+    'targets': [
+        31, 28
+    ]
+}, {
+    'text': 'Does it cost the viewer money to use/get the promoting thing?',
+    'type': 'yesno',
+    'targets': [
+        27, 30
+    ]
+}, {
+    'text': 'Merchandise? Charity? Patren? Donations?',
+    'type': 'yesno',
+    'targets': [
+        25, 30
+    ]
+}, {
+    'text': 'Unpaid/ Self Promotion'
+}, {
+    'text': 'Is the segment located at or near the end of the video?',
+    'type': 'yesno',
+    'targets': [
+        31, 32
+    ]
+}, {
+    'text': 'Would someone watching the video probably want to know about this?',
+    'type': 'yesno',
+    'targets': [
+        1, 18
+    ]
+}, {
+    'text': 'Endcards/ Credits'
+}, {
+    'text': 'Is this segment identical to other segments in videos by the same creator?',
+    'type': 'yesno',
+    'targets': [
+        33, 1
+    ]
+}, {
+    'text': 'Is this a disclaimer intended to protect the viewer? (Epilepsy, Trigger Warning, 18+)',
+    'type': 'yesno',
+    'targets': [
+        1, 34
+    ]
+}, {
+    'text': 'Is the segment skipping to the main point of the video?',
+    'type': 'yesno',
+    'targets': [
+        1, 35
+    ]
+}, {
+    'text': 'Intermission/ Intro Animation'
+}, {
+    'text': 'Is the segment present in the official music? (Spotify Version)',
+    'type': 'yesno',
+    'targets': [
+        1, 37
+    ]
+}, {
+    'text': 'Is part of the song playing in the background of the segment?',
+    'type': 'yesno',
+    'targets': [
+        1, 38
+    ]
+}, {
+    'text': 'Music: Non-Music Section'
+}, {
+    'text': 'Sponsor'
 }];
--- layout.md
+++ layout.md
@@ -1,39 +1,40 @@
-0 | Is the video better off without skipping the segment?
+0 | 1, 2 | Is the video better off without skipping the segment?
 1 | Do not submit anything
-2 | Is the segment shorter than one second? Is the transition really bad?
-3 | Is there any content in the segment?
-4 | Is there a sponsor in the segment?
-5 | Are they asking you to like, subscribe, turn on notifications, comment and share?
-6 | Are they promoting something
-7 | Does this segment contain an animation or still frame? (May contain audio)
-8 | Is this a unique spoken introduction? Or a unique skit?
-9 | Is this a time-lapse or slomo clip? Is it showing preperation/ cleanup of some sort?
-10 | Is music the primary aspect of the video
-11 | Is this video part of a series
-12 | Does this segment appear in the next or previous videos?
-13 | Does content from this segment appear later or earlier in this video?
-14 | Is there a spoken summary that is not present in the other appearance?
+2 | 1, 3 |  Is the segment shorter than one second? Is the transition really bad?
+3 | 12, 4 | Is there any content in the segment?
+4 | 16, 5 | Is there a sponsor in the segment?
+5 | 23, 6 | Are they asking you to like, subscribe, turn on notifications, comment and share?
+6 | 26, 7 | Are they promoting something
+7 | 29, 8 | Does this segment contain an animation or still frame? (May contain audio)
+8 | 1, 9 | Is this a unique spoken introduction? Or a unique skit?
+9 | 1, 10 | Is this a time-lapse or slomo clip? Is it showing preperation/ cleanup of some sort?
+10 | 36, 1 | Is music the primary aspect of the video
+11 | 12, 1 | Is this video part of a series
+12 | 14, 1 | Does this segment appear in the next or previous videos?
+13 | 14, 11 | Does content from this segment appear later or earlier in this video?
+14 | 1, 15 | Is there a spoken summary that is not present in the other appearance?
 15 | Preview/Recap
-16 | Is the video primarily about the sponsor or could this be cause of bias?
-17 | Is it obvious the sponsor is owned by the same company/person?
-18 | Is it obvious the creator is being paid to promote this? (or received free stuff)
-19 | Is the sponsor Nebula or Curiosity Stream
-20 | Does the creator have exclusive videos on the platform
-21 | Do they switch between prompting the platform and their exclusives rapidly
+16 | 1, 17 | Is the video primarily about the sponsor or could this be cause of bias?
+17 | 25, 18 | Is it obvious the sponsor is owned by the same company/person?
+18 | 19, 25 | Is it obvious the creator is being paid to promote this? (or received free stuff)
+19 | 20, 39 | Is the sponsor Nebula or Curiosity Stream
+20 | 21, 39 | Does the creator have exclusive videos on the platform
+21 | 39, 22 | Do they switch between prompting the platform and their exclusives rapidly
 22 | When prompting their exclusive self promo, While promoting the platform: sponsor
-23 | Do they rapidly switch between reminding you to interact and self promotion?
-24 | Interaction reminder
-25 | Are endcards present or would outro make more sense if it was included
-26 | Does it cost the viewer money to use/get the promoted thing?
-27 | Merchandice? Charity? Patreon? Donations
+23 | 25, 24 | Do they rapidly switch between reminding you to interact and self promotion?
+24 | Interaction Reminder
+25 | 31, 28 | Are endcards present or would outro make more sense if it was included
+26 | 27, 30 | Does it cost the viewer money to use/get the promoted thing?
+27 | 25, 30 | Merchandice? Charity? Patreon? Donations
 28 | Unpaid/ Self Promotion
-29 | Is the segment located at or near the end of the video?
-30 | Would someone watching the video probably want to know about this?
+29 | 31, 32 | Is the segment located at or near the end of the video?
+30 | 1, 18 | Would someone watching the video probably want to know about this?
 31 | Endcards/ Credit
-32 | Is this segment identical to other segments in videos by the same creator?
-33 | is this a disclaimer indended to protect the viewer? (Epilepsy, trigger warning, 18+)
-34 | Is the segment skipping to the main point of the video?
+32 | 33, 1 | Is this segment identical to other segments in videos by the same creator?
+33 | 1, 34 | is this a disclaimer indended to protect the viewer? (Epilepsy, trigger warning, 18+)
+34 | 1, 35 | Is the segment skipping to the main point of the video?
 35 | Intermission/ Intro animation
-36 | Is the segment present in the officiail music? (Spotify Version)
-37 | Is part of the song playing in the background of the segment?
+36 | 1, 37 | Is the segment present in the official music? (Spotify Version)
+37 | 1, 38 | Is part of the song playing in the background of the segment?
 38 | Music: Non-Music Section
+39 | Sponsor
